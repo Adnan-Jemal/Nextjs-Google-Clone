0190cd56c0a9719a45f84cafb0e4871b459e66c4
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2023-10-29 14:49:21 +0300

    add homeheader component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HomeHeader from './components/HomeHeader'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hellow google</h1>
+    <main className="">
+      <HomeHeader/>
     </main>
   )
 }
