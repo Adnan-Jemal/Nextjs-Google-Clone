f8ce28438397cfe785863d2edd7c19a3fc5d3153
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2023-11-01 13:11:58 +0300

    add loading effect

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <>
+    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 max-w-[200px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px] "></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[560px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[530px] "></div>
+    </div>
+    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 max-w-[200px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px] "></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[560px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[530px] "></div>
+    </div>
+    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 max-w-[200px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px] "></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[560px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[530px] "></div>
+    </div>
+    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 max-w-[200px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px] "></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[560px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[530px] "></div>
+    </div>
+    <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 max-w-48"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px] "></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[560px]"></div>
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2 max-w-[530px] "></div>
+    </div>
+    </>
+  )
+}
+
+export default Loading
