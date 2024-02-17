22380dbe783370b16267dfbb46bd7700b395a291
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-17 14:56:27 +0300

    added home header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <HomeHeader/>
      
      
     </main>
