4167f9a0736f36e58cd745e64ef06f3f79da2aab
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-18 22:49:11 +0300

    fix error for deploaying

--- components/PaginationBtns.tsx
+++ components/PaginationBtns.tsx
@@ -7,13 +7,13 @@ const PaginationBtns = () => {
   const pathName = usePathname();
   const searchParams = useSearchParams();
   const searchTerm = searchParams?.get("searchTerm");
-  const startIndex = +searchParams.get("start") || 1;
+  const startIndex = searchParams.get("start") || 1;
 
   return (
     <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm: sm:px-0">
-      {startIndex >= 10 && (
+      {Number(startIndex) >= 10 && (
         <Link
-          href={`${pathName}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
+          href={`${pathName}?searchTerm=${searchTerm}&start=${Number(startIndex) - 10}`}
         >
           <div className="flex flex-col items-center hover:underline">
             <BsChevronLeft className="h-5" />
@@ -21,9 +21,9 @@ const PaginationBtns = () => {
           </div>
         </Link>
       )}
-      {startIndex <= 90 && (
+      {Number(startIndex) <= 90 && (
         <Link
-          href={`${pathName}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
+          href={`${pathName}?searchTerm=${searchTerm}&start=${Number(startIndex) + 10}`}
         >
           <div className="flex flex-col items-center hover:underline">
             <BsChevronRight className="h-5" />
