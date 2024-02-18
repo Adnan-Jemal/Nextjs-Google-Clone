aac1c3d60c31ecb45f809b659a41ab1dd89d6f21
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-18 14:58:13 +0300

    handled possible errors

---
+++ app/search/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+
+
+const SearchError = () => {
+ 
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">Something went wrong!</h1>
+      <button className="text-blue-500">Try again</button>
+    </div>
+  );
+};
+
+export default SearchError;
--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { error } from "console";
+import Link from "next/link";
+import React from "react";
 
-const WebSearchPage = () => {
+const WebSearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { searchTerm: string };
+}) => {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  if (!res.ok) throw new Error("something went wrong");
+  const data = await res.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10 text-center">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web for images for something else{" "}
+          <Link href={"/"} className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return (
-    <div>WebSearchPage</div>
-  )
-}
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
+};
 
-export default WebSearchPage
+export default WebSearchPage;
